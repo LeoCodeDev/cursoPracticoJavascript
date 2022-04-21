5d66dabed38781dcf969a5c48d7747af4377ff59
code-from-scratch1 <alb.mar.cta@gmail.com>
2022-04-20 20:28:58 -0500

    finalizada clase 8

--- figurasF.js
+++ figurasF.js
@@ -6,8 +6,8 @@ function perimetroCuadrado(lado){
     return 'El perimetro del cuadrado es: ' + (lado * 4) + 'cm';
 }
 
-function perimetroTriagulo(lado1, lado2, base){
-    return 'El perimetro del triangulo es: ' + (lado1 + lado2 + base) + 'cm';
+function perimetroTriangulo(lado1, lado2, base){
+    return 'El perimetro del triangulo es: ' + (parseInt(lado1) + parseInt(lado2) + parseInt(base)) + 'cm';
 }
 
 function areaTriangulo(altura, base){
@@ -24,4 +24,48 @@ function perimetroCirculo(radio){
 
 function areaCirculo(radio){
     return'El area del circulo es: ' + ((radio * radio)* Math.PI) + 'cm2'
+}
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("ladoC");
+    const value = input.value;
+    alert(perimetroCuadrado(value));
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("ladoC");
+    const value = input.value;
+    alert(areaCuadrado(value));
+}
+
+function calcularPerimetroCirculo(){
+    const input = document.getElementById("radioC");
+    const value = input.value;
+    alert(perimetroCirculo(value));
+}
+
+function calcularAreaCirculo(){
+    const input = document.getElementById("radioC");
+    const value = input.value;
+    alert(areaCirculo(value));
+}
+
+function calcularPerimetroTriangulo(){
+    const input1 = document.getElementById("ladoT1");
+    const value1 = input1.value;
+    const input2 = document.getElementById("ladoT2");
+    const value2 = input2.value;
+    const input3 = document.getElementById("baseT");
+    const value3 = input3.value;
+    alert(perimetroTriangulo(value1, value2, value3));
+}
+
+function calcularAreaTriangulo(){
+    const input1 = document.getElementById("ladoT1");
+    const value1 = input1.value;
+    const input2 = document.getElementById("ladoT2");
+    const value2 = input2.value;
+    const input3 = document.getElementById("baseT");
+    const value3 = input3.value;
+    alert(areaTriangulo(value1, value2, value3));
 }
